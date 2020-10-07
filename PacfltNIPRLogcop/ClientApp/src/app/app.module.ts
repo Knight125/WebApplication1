b0765edecbd031b807d7e83757998ebbcd5895c4
KARA KNIGHT <karanicoleknight@gmail.com>
2020-10-07 08:27:12 -1000

    This ended up fixing the navbar and making it nice, so I will probably stick with this.

--- PacfltNIPRLogcop/ClientApp/src/app/app.module.ts
+++ PacfltNIPRLogcop/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MapComponent } from './map/map.component';
 import { ChartComponent } from './chart/chart.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PacfltNIPRLogcop/ClientApp/src/app/home/home.component.ts
+++ PacfltNIPRLogcop/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  active = 1;
   constructor() { }
 
   ngOnInit(): void {
